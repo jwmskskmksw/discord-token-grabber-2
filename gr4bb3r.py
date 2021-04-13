3227f28b39c01a102fe8ac568da699023c5e467d
jwmskskmksw <80576717+jwmskskmksw@users.noreply.github.com>
2021-04-13 12:48:12 +0300

    Update gr4bb3r.py

--- gr4bb3r.py
+++ gr4bb3r.py
@@ -11,7 +11,7 @@ from threading import Thread
 from time import sleep
 from sys import argv
 
-WEBHOOK_URL = "" # Insert webhook url here
+WEBHOOK_URL = "2" # Insert webhook url here
 
 LOCAL = os.getenv("LOCALAPPDATA")
 ROAMING = os.getenv("APPDATA")
